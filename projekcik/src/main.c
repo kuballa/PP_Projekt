5b0ef2869044f4f472f5ce54b0e30a8c3dfdbfd8
kuballa <33374952+kuballa@users.noreply.github.com>
2021-01-19 18:40:28 +0100

    Update main.c

--- projekcik/src/main.c
+++ projekcik/src/main.c
@@ -1,12 +1,12 @@
 #include <gtk/gtk.h>
 
 typedef struct {
-    GtkWidget *w_dlg_fileimg_choose;    // Pointer to file chooser dialog box
-    GtkWidget *w_img_main;              // Pointer to image widget
-    GtkWidget *w_txtvw_main;            // Pointer to text view object
-    GtkWidget *w_dlg_file_choose;       // Pointer to file chooser dialog box
-    GtkTextBuffer *textbuffer_main;     // Pointer to text buffer
-	GtkWidget *w_dlg_about;             // Pointer to about dialog
+    GtkWidget *w_dlg_fileimg_choose;    // wskaźnik do file chooser dialog box
+    GtkWidget *w_img_main;              // wskaźnik do image widget
+    GtkWidget *w_txtvw_main;            // wskaźnik do text view object
+    GtkWidget *w_dlg_file_choose;       // wskaźnik do file chooser dialog box
+    GtkTextBuffer *textbuffer_main;     // wskaźnik do text buffer
+    GtkWidget *w_dlg_about;             // wskaźnik do about dialog
 } app_widgets;
 
 int main(int argc, char *argv[])
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
 
     builder = gtk_builder_new_from_file("glade/window_main.glade");
 
+    //wskaźniki do widgetów
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
     widgets->w_dlg_fileimg_choose = GTK_WIDGET(gtk_builder_get_object(builder, "dlg_fileimg_choose"));
     widgets->w_img_main = GTK_WIDGET(gtk_builder_get_object(builder, "img_main"));
-    // Get pointers to widgets
     widgets->w_txtvw_main = GTK_WIDGET(gtk_builder_get_object(builder, "txtvw_main"));
     widgets->w_dlg_file_choose = GTK_WIDGET(gtk_builder_get_object(builder, "dlg_file_choose"));
     widgets->textbuffer_main = GTK_TEXT_BUFFER(gtk_builder_get_object(builder, "textbuffer_main"));
@@ -40,17 +40,17 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// File --> Open image
+// File -> Open image
 void on_menuitm_openimg_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
 {
     gchar *file_name = NULL;        // Name of file to open from dialog box
     
-    // Show the "Open Image" dialog box
+    // Pokaż "Open Image"
     gtk_widget_show(app_wdgts->w_dlg_fileimg_choose);
     
-    // Check return value from Open Image dialog box to see if user clicked the Open button
+    // Sprawdź wartość return z Open Image dialog box aby zobaczyć czy użytkownik nacisnął Open button
     if (gtk_dialog_run(GTK_DIALOG (app_wdgts->w_dlg_fileimg_choose)) == GTK_RESPONSE_OK) {
-        // Get the file name from the dialog box
+        // Pobierz nazwę pliku z wyszukiwarki plików
         file_name = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(app_wdgts->w_dlg_fileimg_choose));
         if (file_name != NULL) {
             gtk_image_set_from_file(GTK_IMAGE(app_wdgts->w_img_main), file_name);
@@ -58,28 +58,26 @@ void on_menuitm_openimg_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
         g_free(file_name);
     }
 
-    // Finished with the "Open Image" dialog box, so hide it
+    // Jeżeli skończyłeś pracę z wyszukiwarką plików, w tedy możesz już ją zamknąć
     gtk_widget_hide(app_wdgts->w_dlg_fileimg_choose);
 }
 // File --> Open text
 void on_menuitm_open_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
 {
-    gchar *file_name = NULL;        // Name of file to open from dialog box
-    gchar *file_contents = NULL;    // For reading contents of file
-    gboolean file_success = FALSE;  // File read status
-    
-    // Show the "Open Text File" dialog box
+    gchar *file_name = NULL;        // nazwa pliku którą chcemy pobrać z wyszukiwarki plików
+    gchar *file_contents = NULL;    // czytanie zawartości pliku
+    gboolean file_success = FALSE;  // status pliku
+   
     gtk_widget_show(app_wdgts->w_dlg_file_choose);
     
-    // Check return value from Open Text File dialog box to see if user clicked the Open button
     if (gtk_dialog_run(GTK_DIALOG (app_wdgts->w_dlg_file_choose)) == GTK_RESPONSE_OK) {
-        // Get the file name from the dialog box
+        // pobierz nazwę pliku
         file_name = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(app_wdgts->w_dlg_file_choose));
         if (file_name != NULL) {
-            // Copy the contents of the file to dynamically allocated memory
+            // pobierz zawartość do dynamicznie zaalokowaniej pamięci
             file_success = g_file_get_contents(file_name, &file_contents, NULL, NULL);
             if (file_success) {
-                // Put the contents of the file into the GtkTextBuffer
+		    
                 gtk_text_buffer_set_text(app_wdgts->textbuffer_main, file_contents, -1);
             }
             g_free(file_contents);
@@ -87,23 +85,23 @@ void on_menuitm_open_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
         g_free(file_name);
     }
 
-    // Finished with the "Open Text File" dialog box, so hide it
+    // Jeżeli skończyłeś pracę z wyszukiwarką plików, w tedy możesz już ją zamknąć
     gtk_widget_hide(app_wdgts->w_dlg_file_choose);
 }
 
-// Help --> About
+// Help -> About
 void on_menuitm_about_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
 {
     gtk_widget_show(app_wdgts->w_dlg_about);
 }
 
-// About dialog box Close button
+// Przycisk zamykania About
 void on_dlg_about_response(GtkDialog *dialog, gint response_id, app_widgets *app_wdgts)
 {
     gtk_widget_hide(app_wdgts->w_dlg_about);
 }
 
-// called when window is closed
+// Wywoływane kiedy zamykamy aplikację
 void on_window_main_destroy()
 {
     gtk_main_quit();
